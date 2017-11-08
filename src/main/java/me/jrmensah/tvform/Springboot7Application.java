5d38c7c274040d1868cf6874a078328ef7df17f0
jrmensah <jerilynmensah@gmail.com>
2017-11-08 11:13:26 -0500

    Tv Show form app

---
+++ src/main/java/me/jrmensah/tvform/Springboot7Application.java
@@ -0,0 +1,12 @@
+package me.jrmensah.tvform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot7Application.class, args);
+	}
+}
